c855d030d01642f78875d30fc2f836ec87327c1a
Reza Saputra <129853013+rezasaputra4250@users.noreply.github.com>
2024-11-28 14:58:37 +0700

    Mengubah file: repair_assignments/delete.php

--- repair_assignments/delete.php
+++ repair_assignments/delete.php
@@ -2,11 +2,10 @@
 include('../config/database.php');
 
 // Handle repair assignment deletion
-if (isset($_GET['id']) && isset($_GET['employee_id'])) {
-  $repair_id = $_GET['id'];
-  $employee_id = $_GET['employee_id'];
+if (isset($_GET['repair_id'])) {
+  $repair_id = $_GET['repair_id'];
 
-  $sql = "DELETE FROM repair_assignments WHERE repair_id = $repair_id AND employee_id = $employee_id";
+  $sql = "DELETE FROM repair_assignments WHERE repair_id = $repair_id";
 
   if ($conn->query($sql) === TRUE) {
     header("Location: index.php"); // Redirect to the list after successful deletion
